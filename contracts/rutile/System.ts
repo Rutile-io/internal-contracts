cfd4240a0f591cd0be40fe81a911f41754b2236a
Franklin Waller <franklin.dijks@gmail.com>
2019-06-25 16:45:58 +0200

    feat(api): Add Keccak256 support

--- contracts/rutile/System.ts
+++ contracts/rutile/System.ts
@@ -1,30 +1,36 @@
 import { call, getReturnDataSize, returnDataCopy } from "../utils/env";
 import { printMemHex, print32, printString } from "../utils/debug";
-import { reverseBytes } from '../utils/reverseBytes';
-import { debug } from "./Debug";
 import { u256 } from '../../node_modules/bignum/assembly/integer/u256';
 import StringMemory from "./utils/StringMemory";
-import toAddress, { toAddressPointer } from "./utils/toAddress";
+import { toAddressPointer } from "./utils/toAddress";
+import { debug } from "./Debug";
 
 let KECCAK_ADDRESS = u256.fromU64(9);
 
 class System {
-    keccak256(value: string): void {
+    keccak256(value: string): ArrayBuffer {
         let ptrAddress = toAddressPointer(KECCAK_ADDRESS);
         const valueMem = new StringMemory(value);
 
+        debug.print('Well fuuuuuu ');
+        debug.print(value);
+        printString(valueMem.pointer, valueMem.length);
+
+        // Calls the system contract for Keccak256
         let returnCode = call(8000000, ptrAddress, 0, valueMem.pointer, valueMem.length);
 
+        // 1 Means the code execution has failed.
         if (returnCode === 1) {
-            return;
+            return new ArrayBuffer(0);
         }
 
         let returnDataSize = getReturnDataSize();
-        let ptrDataCopy = __alloc(returnDataSize, 0);
-
+        let ptrDataCopy = __alloc(returnDataSize, idof<ArrayBuffer>());
         returnDataCopy(ptrDataCopy, 0, returnDataSize);
 
-        printMemHex(ptrDataCopy, returnDataSize);
+        let resultBuffer = changetype<ArrayBuffer>(ptrDataCopy);
+
+        return resultBuffer;
     }
 }
 
